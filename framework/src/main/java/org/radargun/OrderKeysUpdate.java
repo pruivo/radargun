31d19a4a724b8d03ca7e2076c57a2724ec579a4e
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2012-07-09 19:04:03 +0100

    * refactor some code

--- framework/src/main/java/org/radargun/OrderKeysUpdate.java
+++ framework/src/main/java/org/radargun/OrderKeysUpdate.java
@@ -1,107 +1,214 @@
 package org.radargun;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.Closeable;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * // TODO: Document this
  *
- * @author pruivo
- * @since 4.0
+ * @author Pedro Ruivo
+ * @since 1.1
  */
 public class OrderKeysUpdate {
 
+   private static final String FILE_PREFIX = ".keys";
+
    public static void main(String[] args) {
+      Log log = LogFactory.getLog(OrderKeysUpdate.class);
+
       if (args.length == 0) {
-         System.err.println("Error. file expected");
+         log.fatal("File paths are expected as arguments");
          System.exit(1);
       }
+      log.info("Start processing file in " + Arrays.asList(args));
+      long start = System.currentTimeMillis();
+      
+      for (String s : args) {
+         new ProcessLogFile(s).run();
+      }
+      log.info("Finish processing files. Duration (ms) is " + (System.currentTimeMillis() - start));
+      System.exit(0);
+   }
 
-      HashMap<String, List<String>> allKeys = new HashMap<String, List<String>>();
+   private static class ProcessLogFile implements Runnable {
 
-      try {
-         BufferedReader br = new BufferedReader(new FileReader(args[0]));
+      private final String filePath;
+      private final Log log = LogFactory.getLog(ProcessLogFile.class);
+      private final Map<String, List<String>> allKeysUpdates;
 
-         String line;
-         //11:08:56,034 TRACE [ReadCommittedEntry] Updating entry (key=KEY_0 removed=false valid=true changed=true created=true value=<value> newVersion=null]
-         while ((line = br.readLine()) != null) {
-            if (!line.contains("Updating entry")) {
-               continue;
-            }
+      private ProcessLogFile(String filePath) {
+         this.filePath = filePath;
+         this.allKeysUpdates = new TreeMap<String, List<String>>();
+      }
 
-            String key = getKey(line);
-            String value = getValue(line);
-            String version = getVersion(line);
+      @Override
+      public void run() {
+         log.info("Start analyzing log file in " + filePath);
+         analyze();
+         log.info("Start writing output to " + filePath + FILE_PREFIX);
+         writeResult();
+      }
 
-            if (value.length() > 10) {
-               value = value.substring(0, 10);
-            }
-
-            List<String> list = allKeys.get(key);
-            if (list == null) {
-               list = new LinkedList<String>();
-               allKeys.put(key, list);
-            }
-            list.add(value + ":" + version);
-
-
+      private void analyze() {
+         BufferedReader reader = getBufferedReader();
+         if (reader == null) {
+            return;
          }
-         br.close();
-      } catch (FileNotFoundException e) {
-         e.printStackTrace();  // TODO: Customise this generated block
-      } catch (IOException e) {
-         e.printStackTrace();  // TODO: Customise this generated block
-      }
 
-      for (Map.Entry<String, List<String>> entry : allKeys.entrySet()) {
-         System.out.print(entry.getKey());
-         for (String s : entry.getValue()) {
-            System.out.print("|");
-            System.out.print(s);
+         try {
+            String line;
+            //11:08:56,034 TRACE [ReadCommittedEntry] Updating entry (key=KEY_0 removed=false valid=true changed=true created=true value=<value> newVersion=null]
+            while ((line = reader.readLine()) != null) {
+               if (!line.contains("Updating entry")) {
+                  continue;
+               }
+
+               String key = getKey(line);
+               String value = getValue(line);
+               String version = getVersion(line);
+
+               if (value.length() > 10) {
+                  value = value.substring(0, 10);
+               }
+
+               log.trace("Analyzing line... key=" + key + ", value=" + value + ", version=" + version);
+
+               List<String> list = allKeysUpdates.get(key);
+               if (list == null) {
+                  list = new LinkedList<String>();
+                  allKeysUpdates.put(key, list);
+               }
+               list.add(value + ":" + version);
+            }
+         } catch (IOException e) {
+            if (log.isDebugEnabled()) {
+               log.debug("IOException while reading " + filePath, e);
+            } else {
+               log.error("IOException while reading " + filePath + ". " + e.getMessage());
+            }
+         } finally {
+            close(reader);
          }
-         System.out.println();
       }
-   }
 
-   private static String getKey(String key) {
-      return getSubstringBetween("key=", " removed=", key);
-   }
-
-   private static String getValue(String value) {
-      return getSubstringBetween("value=", " newVersion=", value);
-   }
-
-   private static String getVersion(String version) {
-      String aux = getSubstringBetween("newVersion=", "]", version);
-      if (aux != null && aux.contains("version=")) {
-         return getSubstringBetween("version=", "}", aux);
+      private void writeResult() {
+         BufferedWriter writer = getBufferedWriter();
+         if (writer == null) {
+            return;
+         }
+         try {
+            for (Map.Entry<String, List<String>> entry : allKeysUpdates.entrySet()) {
+               writer.write(entry.getKey());
+               writer.write("==>");
+               for (String s : entry.getValue()) {
+                  writer.write(s);
+                  writer.write("|");
+               }
+               writer.newLine();
+               writer.flush();
+            }
+         } catch (IOException e) {
+            if (log.isDebugEnabled()) {
+               log.debug("IOException while writing " + filePath + FILE_PREFIX, e);
+            } else {
+               log.error("IOException while reading " + filePath + FILE_PREFIX + ". " + e.getMessage());
+            }
+         } finally {
+            close(writer);
+         }
       }
-      return aux;
-   }
 
-   private static String getSubstringBetween(String begin, String end, String line) {
-      int beginIdx = begin != null ? line.indexOf(begin) : 0;
-      if (beginIdx == -1) {
+      private BufferedReader getBufferedReader() {
+         try {
+            return new BufferedReader(new FileReader(filePath));
+         } catch (FileNotFoundException e) {
+            if (log.isDebugEnabled()) {
+               log.debug(filePath + " not found!", e);
+            } else {
+               log.fatal(filePath + " not found!" + e.getMessage());
+            }
+         }
          return null;
       }
-      beginIdx += begin != null ? begin.length() : 0;
 
-      int endIdx = end != null ? line.indexOf(end, beginIdx) : line.length();
-
-      if (endIdx == -1) {
+      private BufferedWriter getBufferedWriter() {
+         try {
+            return new BufferedWriter(new FileWriter(filePath + FILE_PREFIX));
+         } catch (FileNotFoundException e) {
+            if (log.isDebugEnabled()) {
+               log.debug(filePath + " not found!", e);
+            } else {
+               log.fatal(filePath + " not found!" + e.getMessage());
+            }
+         } catch (IOException e) {
+            if (log.isDebugEnabled()) {
+               log.debug("IOException while opening " + filePath + FILE_PREFIX, e);
+            } else {
+               log.fatal("IOException while opening " + filePath + FILE_PREFIX + ". " + e.getMessage());
+            }
+         }
          return null;
       }
 
-      if (beginIdx > endIdx || endIdx > line.length()) {
-         return null;
+      private void close(Closeable closeable) {
+         try {
+            closeable.close();
+         } catch (IOException e) {
+            if (log.isDebugEnabled()) {
+               log.debug("Error closing reader/writer for " + filePath, e);
+            } else {
+               log.warn("Error closing reader/writer for " + filePath + ". " + e.getMessage());
+            }
+         }
       }
 
-      return line.substring(beginIdx, endIdx);
+      private String getKey(String key) {
+         return getSubstringBetween("key=", " removed=", key);
+      }
+
+      private String getValue(String value) {
+         return getSubstringBetween("value=", " newVersion=", value);
+      }
+
+      private String getVersion(String version) {
+         String aux = getSubstringBetween("newVersion=", "]", version);
+         if (aux != null && aux.contains("version=")) {
+            return getSubstringBetween("version=", "}", aux);
+         }
+         return aux;
+      }
+
+      private String getSubstringBetween(String begin, String end, String line) {
+         int beginIdx = begin != null ? line.indexOf(begin) : 0;
+         if (beginIdx == -1) {
+            return null;
+         }
+         beginIdx += begin != null ? begin.length() : 0;
+
+         int endIdx = end != null ? line.indexOf(end, beginIdx) : line.length();
+
+         if (endIdx == -1) {
+            return null;
+         }
+
+         if (beginIdx > endIdx || endIdx > line.length()) {
+            return null;
+         }
+
+         return line.substring(beginIdx, endIdx);
+      }
    }
 }
