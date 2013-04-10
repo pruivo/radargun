37ad2690187ee732bd5ddded08006342dde97679
Diego Didona <didona@gsd.inesc-id.pt>
2013-04-10 19:58:44 +0100

    * Trying new has

--- framework/src/main/java/org/radargun/stages/WebSessionWarmupStage.java
+++ framework/src/main/java/org/radargun/stages/WebSessionWarmupStage.java
@@ -17,54 +17,58 @@ import java.util.Set;
  */
 public class WebSessionWarmupStage extends WebSessionBenchmarkStage {
 
-   private static final Set<String> WARMED_UP_CONFIGS = new HashSet<String>(2);
+    private static final Set<String> WARMED_UP_CONFIGS = new HashSet<String>(2);
 
-   @Override
-   public DistStageAck executeOnSlave() {
-      DefaultDistStageAck result = new DefaultDistStageAck(slaveIndex, slaveState.getLocalAddress());
-      this.cacheWrapper = slaveState.getCacheWrapper();
-      if (cacheWrapper == null) {
-         log.info("Not running test on this slave as the wrapper hasn't been configured.");
-         return result;
-      }
-
-      String configName = cacheWrapper.getClass().getName() + " - " + cacheWrapper.getInfo();
-
-      if (!WARMED_UP_CONFIGS.contains(configName)) {
-
-         try {
-            long startTime = System.currentTimeMillis();
-            doWork();
-            long duration = System.currentTimeMillis() - startTime;
-            log.info("The warmup took: " + (duration / 1000) + " seconds.");
-            result.setPayload(duration);
-            WARMED_UP_CONFIGS.add(configName);
+    @Override
+    public DistStageAck executeOnSlave() {
+        DefaultDistStageAck result = new DefaultDistStageAck(slaveIndex, slaveState.getLocalAddress());
+        this.cacheWrapper = slaveState.getCacheWrapper();
+        if (cacheWrapper == null) {
+            log.info("Not running test on this slave as the wrapper hasn't been configured.");
             return result;
-         } catch (Exception e) {
-            log.warn("Exception while running " + getClass().getSimpleName(), e);
-            result.setError(true);
-            result.setRemoteException(e);
+        }
+
+        String configName = cacheWrapper.getClass().getName() + " - " + cacheWrapper.getInfo();
+
+        if (!WARMED_UP_CONFIGS.contains(configName)) {
+            if (slaveIndex != 0) {
+                log.warn("Skipping Warmup as I am not the master node");
+            } else {
+                log.warn("Warming up as I am the master");
+            }
+            try {
+                long startTime = System.currentTimeMillis();
+                doWork();
+                long duration = System.currentTimeMillis() - startTime;
+                log.info("The warmup took: " + (duration / 1000) + " seconds.");
+                result.setPayload(duration);
+                WARMED_UP_CONFIGS.add(configName);
+                return result;
+            } catch (Exception e) {
+                log.warn("Exception while running " + getClass().getSimpleName(), e);
+                result.setError(true);
+                result.setRemoteException(e);
+                return result;
+            }
+        } else {
+            log.info("Skipping warmup, this has already been done for this configuration on this node.");
             return result;
-         }
-      } else {
-         log.info("Skipping warmup, this has already been done for this configuration on this node.");
-         return result;
-      }
-   }
+        }
+    }
 
-   @Override
-   public boolean processAckOnMaster(List<DistStageAck> acks, MasterState masterState) {
-      logDurationInfo(acks);
-      for (DistStageAck ack : acks) {
-         DefaultDistStageAck dAck = (DefaultDistStageAck) ack;
-         if (dAck.isError())
-            log.warn("Caught error on slave " + dAck.getSlaveIndex() + " when running " + getClass().getSimpleName() + ".  Error details:" + dAck.getErrorMessage());
-      }
-      return true;
-   }
+    @Override
+    public boolean processAckOnMaster(List<DistStageAck> acks, MasterState masterState) {
+        logDurationInfo(acks);
+        for (DistStageAck ack : acks) {
+            DefaultDistStageAck dAck = (DefaultDistStageAck) ack;
+            if (dAck.isError())
+                log.warn("Caught error on slave " + dAck.getSlaveIndex() + " when running " + getClass().getSimpleName() + ".  Error details:" + dAck.getErrorMessage());
+        }
+        return true;
+    }
 
-   @Override
-   public String toString() {
-      return "Warmup for " + super.toString();
-   }
+    @Override
+    public String toString() {
+        return "Warmup for " + super.toString();
+    }
 }
